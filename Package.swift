940349b50795f51e6e92f0defa8536727e07ab99
Scott Marchant <scottm@passivelogic.com>
2025-06-25 15:27:58 -0600

    chore: Set up initial swift package manifest file.

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version: 6.1
+
+import PackageDescription
+
+let package = Package(
+    name: "dispatch-async",
+    products: [
+        .library(
+            name: "DispatchAsync",
+            targets: ["DispatchAsync"]),
+    ],
+    targets: [
+        .target(
+            name: "DispatchAsync"),
+        .testTarget(
+            name: "DispatchAsyncTests",
+            dependencies: ["DispatchAsync"]
+        ),
+    ]
+)
