64036b926dd23a96ec8af3e9df9ef09497d753b5
Scott Marchant <scottm@passivelogic.com>
2025-06-27 13:38:44 -0600

    feat: Implement DispatchTimeInterval using Swift Concurrency.

---
+++ Sources/DispatchAsync/PackageConstants.swift
@@ -0,0 +1,15 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 Apple Inc. and the Swift project authors
+// Licensed under Apache License v2.0 with Runtime Library Exception
+//
+// See https://swift.org/LICENSE.txt for license information
+// See https://swift.org/CONTRIBUTORS.txt for the list of Swift project authors
+//
+//===----------------------------------------------------------------------===//
+
+package let NSEC_PER_SEC:  UInt64 = 1_000_000_000
+package let NSEC_PER_MSEC: UInt64 = 1_000_000
+package let NSEC_PER_USEC: UInt64 = 1_000
