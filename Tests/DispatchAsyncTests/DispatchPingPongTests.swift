f9cf1e00d9496b4504a920e9f6cb26617eec6d9d
Scott Marchant <scottm@passivelogic.com>
2025-07-09 16:59:42 -0600

    chore: Update availability and other compilation issues after latest rebase.

--- Tests/DispatchAsyncTests/DispatchPingPongTests.swift
+++ Tests/DispatchAsyncTests/DispatchPingPongTests.swift
@@ -1,6 +1,21 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 PassiveLogic, Inc.
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+@_spi(DispatchAsync) import DispatchAsync
 import Testing
 
-@testable import DispatchAsync
+private typealias DispatchQueue = DispatchAsync.DispatchQueue
 
 /// Ping-Pong queue test  is adapted from the test
 /// [dispatch_pingpong.c in libdispatch](https://github.com/swiftlang/swift-corelibs-libdispatch/blob/main/tests/dispatch_pingpong.c).
--- Tests/DispatchAsyncTests/DispatchTimeIntervalTests.swift
+++ Tests/DispatchAsyncTests/DispatchTimeIntervalTests.swift
@@ -1,6 +1,21 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 PassiveLogic, Inc.
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
+@_spi(DispatchAsync) import DispatchAsync
 import Testing
 
-@testable import DispatchAsync
+private typealias DispatchTimeInterval = DispatchAsync.DispatchTimeInterval
 
 @Test
 func dispatchTimeIntervalEquality() throws {
