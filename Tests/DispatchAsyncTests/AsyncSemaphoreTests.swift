a5c25531b9ecacab6fe548830200a27f2c9ee063
Scott Marchant <scottm@passivelogic.com>
2025-07-09 17:15:41 -0600

    refactor: Consolidate semaphore tests to single file.

--- Tests/DispatchAsyncTests/AsyncSemaphoreTests.swift
+++ Tests/DispatchAsyncTests/AsyncSemaphoreTests.swift
@@ -1,3 +1,17 @@
+//===----------------------------------------------------------------------===//
+//
+// This source file is part of the Swift.org open source project
+//
+// Copyright (c) 2025 PassiveLogic, Inc.
+// Licensed under Apache License v2.0
+//
+// See LICENSE.txt for license information
+// See CONTRIBUTORS.txt for the list of Swift.org project authors
+//
+// SPDX-License-Identifier: Apache-2.0
+//
+//===----------------------------------------------------------------------===//
+
 import Testing
 
 @testable import DispatchAsync
@@ -39,4 +53,43 @@ func asyncSemaphoreWaitSignal() async throws {
     await semaphore.wait()
 
     #expect(didEnterCriticalSection)   // waiter must have run
+}
+
+@Test func basicAsyncSemaphoreTest() async throws {
+    nonisolated(unsafe) var sharedPoolCompletionCount = 0
+    sharedPoolCompletionCount = 0 // Reset to 0 for each test run
+    let totalConcurrentPools = 10
+
+    let semaphore = AsyncSemaphore(value: 1)
+
+    await withTaskGroup(of: Void.self) { group in
+        for _ in 0 ..< totalConcurrentPools {
+            group.addTask {
+                // Wait for any other pools currently holding the semaphore
+                await semaphore.wait()
+
+                // Only one task should mutate counter at a time
+                //
+                // If there are issues with the semaphore, then
+                // we would expect to grab incorrect values here occasionally,
+                // which would result in an incorrect final completion count.
+                //
+                let existingPoolCompletionCount = sharedPoolCompletionCount
+
+                // Add artificial delay to amplify race conditions
+                // Pools started shortly after this "semaphore-locked"
+                // pool starts will run before this line, unless
+                // this pool contains a valid lock.
+                try? await Task.sleep(nanoseconds: 100)
+
+                sharedPoolCompletionCount = existingPoolCompletionCount + 1
+
+                // When we exit this flow, release our hold on the semaphore
+                await semaphore.signal()
+            }
+        }
+    }
+
+    // After all tasks are done, counter should be 10
+    #expect(sharedPoolCompletionCount == totalConcurrentPools)
 }
